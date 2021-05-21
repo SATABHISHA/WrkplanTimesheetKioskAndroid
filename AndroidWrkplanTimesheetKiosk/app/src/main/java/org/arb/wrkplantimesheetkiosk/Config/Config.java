4347431e5fa422afa6a3fcc16b4ee4f34c60d0c6
Satabhisha Roy <satabhisha.innits@gmail.com>
2021-05-22 00:52:56 +0530

    modified

--- AndroidWrkplanTimesheetKiosk/app/src/main/java/org/arb/wrkplantimesheetkiosk/Config/Config.java
+++ AndroidWrkplanTimesheetKiosk/app/src/main/java/org/arb/wrkplantimesheetkiosk/Config/Config.java
@@ -1,6 +1,6 @@
 package org.arb.wrkplantimesheetkiosk.Config;
 
 public class Config {
-//    public static String BaseUrl = "http://14.99.211.60:9012/";
-    public static String BaseUrl = "http://www.erpgovmobile.com/";
+    public static String BaseUrl = "http://14.99.211.60:9012/";
+//    public static String BaseUrl = "http://www.erpgovmobile.com/";
 }
